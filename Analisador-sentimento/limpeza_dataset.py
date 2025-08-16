4f0b7bf97ccf65d133be63aa506ba3db8c83b04c
GabeAlv <gab.santos.ti@gmail.com>
2025-08-16 18:28:05 -0300

    Alterando o código para POO
    Refatorei o código para ser orientado a objeto

--- Analisador-sentimento/limpeza_dataset.py
+++ Analisador-sentimento/limpeza_dataset.py
@@ -8,73 +8,77 @@ import pickle # biblioteca para baixar arquivos
 
 #baixando as stopwords
 nltk.download('stopwords')
-#baixando e carregando o modelo de lematização
+#baixando o modelo de lematização
 spacy.cli.download('pt_core_news_sm')
-nlp = spacy.load('pt_core_news_sm')
 
+class pré_processamento:
+    def __init__(self, data):
+        self.data = data
+
+    # removendo colunas e alterando valores
+    def limpando_dataset(self):
+      print('Limpando o dataset...')
+    #tirando as colunas que nao vamos usar
+      self.data = self.data.drop(columns=['original_index', 'review_text_processed', 'review_text_tokenized', 'kfold_polarity', 'kfold_rating'])
+      self.data['polarity'] = self.data['polarity'].astype('object')
+      valores_nulos = self.data.loc[self.data['polarity'].isnull()] # localizando os registros com valores nulos
+      self.data = self.data.drop(valores_nulos.index) # retirando os registros com valores nulos
+    #alterando o nome das classes
+      self.data.loc[self.data['polarity'] == 1, 'polarity'] = 'Positivo'
+      self.data.loc[self.data['polarity'] == 0, 'polarity'] = 'Negativo'
+      return self
+
+    # método para limpar o texto
+    def limpar_texto_dataset(self):
+        print('Limpando os reviews...')
+        self.data['review_limpo'] = self.data['review_text'].apply(self.limpar_texto) # aplicando a limpeza
+        return self
+
+
+    def limpar_texto(self, texto):
+      texto = re.sub(r'@\w+', '', texto) # remove menções
+      texto = re.sub(r'https?://\S+', '', texto) # remove URLs
+      texto = re.sub(r'[^a-zA-ZÀ-ÿ]', ' ', texto) # remove caracteres nao alfabéticos
+      texto = texto.lower() # colocando os textos no minusculo
+      return texto
+
+    # tirando as stopwords
+    def tirando_stopwords(self):
+        print('Retirando stopwords...')
+        #pegando as stopword do português
+        stop_words = set(stopwords.words('portuguese'))
+        self.data['review_limpo'] = self.data['review_limpo'].apply(lambda x: ' '.join([palavra for palavra in x.split() if palavra not in stop_words])) #aplicando a limpeza
+        return self
+
+    # lematizando os textos (colocando as palavras em sua forma base)
+    def lematizar(self, nlp):
+        print('lematizando...')
+        resultados = []
+        #processando os textos em lote com o spacy para melhor eficiência
+        for doc in nlp.pipe(self.data['review_limpo'].tolist(), batch_size=2000):  # batch_size controla quantos textos são processados por vez
+            # Extrai os lemas (formas base) apenas de substantivos e verbos
+            texto_lematizado = ' '.join([
+                token.lemma_  # Pega o lema da palavra
+                for token in doc  # Para cada token no documento
+                if token.pos_ in ['NOUN', 'VERB']  # Filtra só substantivos e verbos
+            ])
+            resultados.append(texto_lematizado) # Adiciona o texto lematizado à lista de resultados
+        self.data['review_limpo'] = resultados
+        return self.data
+
+nlp = spacy.load('pt_core_news_sm') # carregando o modelo de lematização
+data = None
 try:
-    #carregando o dataset
-    data = pd.read_csv('b2w.csv')
+    data = pd.read_csv('b2w.csv') # pegando o dataset que vamos usar
 except FileNotFoundError:
-    print('Dataset não encontrado')
-
-# removendo colunas e alterando valores
-def limpando_dataset(data):
-#tirando as colunas que nao vamos usar
-  data = data.drop(
-    columns=['original_index', 'review_text_processed', 'review_text_tokenized', 'kfold_polarity', 'kfold_rating'])
-  data['polarity'] = data['polarity'].astype('object')
-  valores_nulos = data.loc[data['polarity'].isnull()] # localizando os registros com valores nulos
-  data = data.drop(valores_nulos.index) # retirando os registros com valores nulos
-#alterando o nome das classes
-  data.loc[data['polarity'] == 1, 'polarity'] = 'Positivo'
-  data.loc[data['polarity'] == 0, 'polarity'] = 'Negativo'
-  return data
-
-# função para limpar os textos
-def limpar_texto(texto):
-  texto = re.sub(r'@\w+', '', texto) # remove menções
-  texto = re.sub(r'https?://\S+', '', texto) # remove URLs
-  texto = re.sub(r'[^a-zA-ZÀ-ÿ]', ' ', texto) # remove caracteres nao alfabéticos
-  texto = texto.lower() # colocando os textos no minusculo
-  return texto
-
-# tirando as stopwords
-def tirando_stopwords(data):
-    #pegando as stopword do português
-    stop_words = set(stopwords.words('portuguese'))
-    data['review_limpo'] = data['review_limpo'].apply(lambda x: ' '.join([palavra for palavra in x.split() if palavra not in stop_words])) #aplicando a limpeza
-    return data
-
-# lematizando os textos (colocando as palavras em sua forma base)
-def lematizar(lista_textos, nlp):
-    resultados = []
-    #processando os textos em lote com o spacy para melhor eficiência
-    for doc in nlp.pipe(lista_textos, batch_size=2000):  # batch_size controla quantos textos são processados por vez
-        # Extrai os lemas (formas base) apenas de substantivos e verbos
-        texto_lematizado = ' '.join([
-            token.lemma_  # Pega o lema da palavra
-            for token in doc  # Para cada token no documento
-            if token.pos_ in ['NOUN', 'VERB']  # Filtra só substantivos e verbos
-        ])
-        resultados.append(texto_lematizado) # Adiciona o texto lematizado à lista de resultados
-    return resultados
-
-def função_principal():
-    print('limpando dataset...')
-    dataset_limpo = limpando_dataset(data) # recebendo o dataset tratado
-    dataset_limpo['review_limpo'] = dataset_limpo['review_text'].apply(limpar_texto) # aplicando a função de limpeza na coluna com os reviews
-    print('tirando as stopwords...')
-    dataset_limpo = tirando_stopwords(dataset_limpo) # retirando as stopwords dos reviews
-    print('lematizando...')
-    dataset_limpo['review_limpo'] = lematizar(dataset_limpo['review_limpo'].tolist(), nlp) # lematizando os reviews
-    return dataset_limpo
+    print('Arquivo não encontrado')
 
 if __name__ == '__main__':
-    data_final = função_principal() # chamando a função principal e recebendo o dataset final
-    colunas_para_salvar = data_final[['review_limpo', 'polarity', 'rating']]
+    pre = pré_processamento(data) # criando uma instância da classe
+    dataset_final = pre.limpando_dataset().limpar_texto_dataset().tirando_stopwords().lematizar(nlp) # utilizando os métodos do objeto
+    colunas_para_salvar = dataset_final[['review_limpo', 'polarity', 'rating']] # guardando as colunas necessárias
+    print('Pré processamento concluído!!')
 
-    # baixando as colunas que vao ser usadas posteriormente (coluna rating para visualização com graficos)
+    # baixando as colunas que vao ser usadas posteriormente (coluna rating para visualização com gráficos)
     with open('colunas.pkl', 'wb') as arquivo:
         pickle.dump(colunas_para_salvar, arquivo)
-
